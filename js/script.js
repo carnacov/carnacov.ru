c3d0504881c740909c13cc1dbe9c6dd779162054
Boris <krnkvb@gmail.com>
2025-05-26 12:34:02 +0700

    Убираем бургер из Script,js

--- js/script.js
+++ js/script.js
@@ -7,28 +7,3 @@ new Swiper('.image-slider', {
 		prevEl: '.swiper-button-prev'
 	},
 });
-
-// Burger
-
-const burger = document?.querySelector('[data-burger]');
-const nav = document?.querySelector('[data-nav]');
-const navItems = nav?.querySelectorAll('a');
-const body = document.body;
-const header = document?.querySelector('.header');
-const headerHeight = header.offsetHeight;
-console.log(headerHeight)
-document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
-
-burger?.addEventListener('click', () => {
-  body.classList.toggle('stop-scroll');
-  burger?.classList.toggle('burger--active');
-  nav?.classList.toggle('nav--visible');
-});
-
-navItems.forEach(el => {
-  el.addEventListener('click', () => {
-    body.classList.remove('stop-scroll');
-  burger?.classList.remove('burger--active');
-  nav?.classList.remove('nav--visible');
-  });
-});
